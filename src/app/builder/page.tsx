30873ca560f43ab1427143a0fbbed41668c4c3b4
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-04-08 00:40:05 +0500

    Little-bit Update

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import ModernTemplate from '../components/templates/ModernTemplate';
 import ProfessionalTemplate from '../components/templates/ProfessionalTemplate';
